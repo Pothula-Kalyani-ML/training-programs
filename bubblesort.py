1c5a312e270cd501050753f67d97a769efc81aee
pothula-dot-kalyani <98814079+pothula-dot-kalyani@users.noreply.github.com>
2022-02-06 16:03:27 +0530

    bubble sort in python

---
+++ bubblesort.py
@@ -0,0 +1,18 @@
+array=list(map(int, input("elements of array:-").strip().split()))
+print(array)
+length=len(array)
+print(length)
+count=length
+while(count): 
+    for i in range(0,count-1):
+        if array[i] > array[i+1]:
+            temp=array[i]
+            array[i]=array[i+1]
+            array[i+1]=temp
+        for i in range(length):
+            print(array[i],end=" ")
+        print(end=" ")
+           
+    print(" ")        
+    count=count-1  
+   
