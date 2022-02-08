2e42e344835ff3d0905cab2d214f76ee7cc18ab4
pothula-dot-kalyani <98814079+pothula-dot-kalyani@users.noreply.github.com>
2022-02-08 16:31:10 +0530

    list and string conversions

---
+++ list_string_conversions.py
@@ -0,0 +1,16 @@
+string1 = " this is the text to be modified"
+string2=""
+array1=[]
+array2=[1,2,3,4]
+array3=[]
+string3="hello"
+array1=string1.split(" ")
+x=len(string3)
+print(array1)
+for i in range(x):
+    array3.append(string3[i])
+print(array3) 
+for i in array2:
+   string2= string2+str(i)
+print(string2)   
+   
